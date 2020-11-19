7fb1b5ea7934a9f43d6b62c176efdf0bd8d38ae5
Nullaferius <Cafercotel@gmail.com>
2020-11-19 03:16:06 +0300

    Added component for detailed post

--- components/detailedPost.js
+++ components/detailedPost.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "../styles/Post.module.css";
+import styles from "../styles/detailedPost.module.css";
 import {
   createMuiTheme,
   makeStyles,
@@ -39,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Post(props) {
+export default function detailedPost(props) {
   const classes = useStyles();
   const [like, setLike] = useState(false);
   const [likeCount, setLikeCount] = useState(parseInt(props.likeCount));
@@ -57,7 +57,6 @@ export default function Post(props) {
             transition: { duration: 0.3 },
           }}
         >
-          <Link href={"forum/posts/" + props.id}>
           <CardHeader
             avatar={
               <Avatar aria-label="recipe" className={classes.avatar}>
@@ -67,7 +66,6 @@ export default function Post(props) {
             title={props.title}
             subheader={<TimeAgo date={props.date} />}
           />
-          </Link>
 
           <CardContent></CardContent>
           <CardActions disableSpacing>
@@ -89,20 +87,19 @@ export default function Post(props) {
                 {likeCount} Likes
               </Typography>
             </IconButton>
-            <Link href={"forum/posts/" + props.id}>
-              <IconButton aria-label="comment">
-                <Comment color="primary" />
 
-                <Typography variant="body2" style={{ marginLeft: "5px" }}>
-                  {props.commentCount} Comments
-                </Typography>
-              </IconButton>
-            </Link>
+            <IconButton aria-label="comment">
+              <Comment color="primary" />
+
+              <Typography variant="body2" style={{ marginLeft: "5px" }}>
+                {props.commentCount} Comments
+              </Typography>
+            </IconButton>
 
             <IconButton
               aria-label="share"
               onClick={() => {
-                navigator.clipboard.writeText(window.location.href + "/posts/" + props.id);
+                navigator.clipboard.writeText(window.location.href);
                 setShareAlert(true);
                 setTimeout(() => {
                   setShareAlert(false);
--- pages/forum/posts/[pid].js
+++ pages/forum/posts/[pid].js
@@ -1,67 +1,65 @@
 import styles from "../../../styles/Forum.module.css";
 import React, { useState } from "react";
-import Post from "../../../components/post";
+import DetailedPost from "../../../components/detailedPost";
 import Head from "next/head";
 import useSWR from "swr";
 import fetcher from "../../../lib/fetch";
 import Typography from "@material-ui/core/Typography";
 import { useRouter } from "next/router";
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import IconButton from "@material-ui/core/IconButton";
-import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 const theme = createMuiTheme({
-    palette: {
-        type: "dark",
-        primary: {
-            main: "#00adb5",
-        },
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#00adb5",
     },
+  },
 });
-
 
 function Pid() {
   const { data, error } = useSWR("/api/dummyposts", fetcher);
-
 
   const router = useRouter();
   const { pid } = router.query;
 
   return (
-      <ThemeProvider theme={theme}>
-    <div>
-      <Head>
-        <title>Forum</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Head>
+          <title>Forum</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-
-      <div className={styles.container}>
+        <div className={styles.container}>
           <Link href="/forum">
-          <IconButton aria-label="back"><ArrowBackIcon color="primary" fontSize="large"/></IconButton>
+            <IconButton aria-label="back">
+              <ArrowBackIcon color="primary" fontSize="large" />
+            </IconButton>
           </Link>
 
-        <div className={styles.postContainer}>
-          {!data && <Typography variant="h2">Loading</Typography>}
-          {data?.posts.post.map((post) => {
-            return (
-              post.id === pid && (
-                <Post
-                  key={post.id}
-                  title={post.title}
-                  likeCount={post.likeCount}
-                  commentCount={post.commentCount}
-                  date={post.date}
-                />
-              )
-            );
-          })}
+          <div className={styles.postContainer}>
+            {!data && <Typography variant="h2">Loading</Typography>}
+            {data?.posts.post.map((post) => {
+              return (
+                post.id === pid && (
+                  <DetailedPost
+                    key={post.id}
+                    title={post.title}
+                    likeCount={post.likeCount}
+                    commentCount={post.commentCount}
+                    date={post.date}
+                  />
+                )
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-      </ThemeProvider>
+    </ThemeProvider>
   );
 }
 
